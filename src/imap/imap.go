081a252f37eacd8576cae62c4207997e66f7cc54
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-10-11 12:13:58 +0200

    Implemented struct and connection method

--- src/imap/imap.go
+++ src/imap/imap.go
@@ -6,3 +6,29 @@
 */
 
 package imap
+
+import (
+	"errors"
+	"github.com/emersion/go-imap/client"
+	"net/url"
+)
+
+type Connection struct {
+	Client *client.Client
+}
+
+func (conn *Connection) Dial(uri *url.URL) error {
+	var err error
+
+	switch uri.Scheme {
+	case "imap":
+		port := uri.Port()
+		conn.Client, err = client.Dial(uri.Host + ":" + port)
+	case "imaps":
+		conn.Client, err = client.DialTLS(uri.Host+":"+uri.Port(), nil)
+	default:
+		return errors.New("Scheme not supported: " + uri.Scheme)
+	}
+
+	return err
+}
