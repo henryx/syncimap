718194b83b9680bd7830c2cded860a797a33ae44
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-10-11 11:55:28 +0200

    Added package for managing IMAP connection

---
+++ src/imap/imap.go
@@ -0,0 +1,8 @@
+/*
+   Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
+   Project       Syncimap
+   Description   A rsync like IMAP syncronization tool
+   License       Apache License 2.0 (see LICENSE for details)
+*/
+
+package imap
